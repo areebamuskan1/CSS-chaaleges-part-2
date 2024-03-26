9f6e8d3f62f962e4689fab35f96225bb131bd140
areeba <163363601+areebamuskan1@users.noreply.github.com>
2024-03-26 12:35:14 -0700

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>challenges</title>
+    <link rel="stylesheet" href="./index.css">
+</head>
+<body>
+    <header>
+        <div class="container"> <!-- New div wrapping header content -->
+            <h1>Hello, my name is <br><span class="heading"><br>Areeba Muskan</span></h1><br></h1>
+            <img src="./123.jpg" alt="Pic of Najaf"/> <!-- Image added after h1 -->
+        </div>
+    </header>
+    <section class="about-me">
+        <div class="container"> <!-- New div wrapping about-me section content -->
+            <h2>I am a <span class="heading2"><b><br>enthaustic</b></span></h2>
+            <p>Crafting elegant websites solutions tailored to your unique needs.</p>
+             <a href="#">See My work</a>
+        </button> 
+    </section>
+
+    <section class="articles">
+        <div class="container"> <!-- New div wrapping articles section content -->
+            <h2>My most recent <span>article</span></h2>
+            <article>
+                <h3>Design systems that scale</h3>
+                <p>Written by Areeba Muskan on March 20, 2024</p>
+                <p>Transforming your ideas into captivating WordPress websites, blending creativity with 
+                    functionality for an unforgettable online presence. 
+                    Let's collaborate to bring your vision to life, pixel by pixel. <span><a href="#" class="link">continue reading</a></span></p>
+                    <a href="#"> See My work</a>
+                </button>
+            </section>
+
+            </article>
+        </div>
+    </section>
+
+</body>
+</html>
+</body>
+</html>
